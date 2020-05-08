7d5d7903d3bf4e34655fde25f1637b88549a142d
Olesia Dmytruk <oj.dmytruk@gmail.com>
2020-05-09 00:10:10 +0300

    начато представление

---
+++ Models/MatrixViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseWorkDO.Models
+{
+    public class MatrixViewModel
+    {
+        public int Dimension { get; set; }
+        public double[][] QAPData { get; set; }
+    }
+}
